622f7b76a6a7f4c38a9de7bbb88fed90226111d2
chandu078 <chandudyavanapelli03@gmail.com>
2026-06-27 20:51:01 +0000

    sm8850-common: Retain old audio AIDL version
    Keep the older audio AIDL interface, as replacing it with a newer
    version results in segmentation faults.
    
    Change-Id: I04ee91f2f11d01c348fc5cb202c6d97a9306eb34
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    
    Signed-off-by: Panz Dev <panz.development@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -119,30 +119,10 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/etc/media_codecs_canoe_v1.xml',
         'vendor/etc/media_codecs_canoe_v2.xml',
     ): blob_fixup()
-        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', '')
-        .regex_replace(r'([ \t]*</MediaCodecs>)', r'    <Include href="media_codecs_dolby_vision.xml" />\n\1'),
-    (
-        'vendor/lib64/hw/android.hardware.bluetooth.audio_sw.so',
-        'vendor/lib64/hw/libaudiocorehal.default.so',
-        'vendor/lib64/hw/libaudiocorehal.qti.so',
-        'vendor/lib64/libaudioplatformconverter.qti.so',
-        'vendor/lib64/libaudioserviceexampleimpl.so',
-        'vendor/lib64/libqtigefar.so',
-        'vendor/lib64/libwfdmmsrc_proprietary.so',
-    ): blob_fixup()
-        .replace_needed('android.hardware.audio.core-V3-ndk.so', 'android.hardware.audio.core-V4-ndk.so'),
-    (
-        'vendor/lib64/hw/android.hardware.bluetooth.audio_sw.so',
-        'vendor/lib64/hw/libaudiocorehal.qti.so',
-        'vendor/lib64/hw/libaudioeffecthal.qti.so',
-        'vendor/lib64/libaudioserviceexampleimpl.so',
-        'vendor/lib64/libqtigefar.so',
-        'vendor/lib64/soundfx/libqcompostprocbundle.so',
-        'vendor/lib64/soundfx/libqcomvisualizer.so',
-        'vendor/lib64/soundfx/libqcomvoiceprocessing.so',
-        'vendor/lib64/soundfx/libvolumelistener.so',
-    ): blob_fixup()
-        .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V4-ndk.so'),
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*
+', '')
+        .regex_replace(r'([ 	]*</MediaCodecs>)', r'    <Include href="media_codecs_dolby_vision.xml" />
+'),
     'vendor/lib64/libaudioserviceexampleimpl.so': blob_fixup()
         .add_needed('libaudioutils_shim.so')
         .add_needed('libbluetooth_audio_session_aidl_shim.so'),
