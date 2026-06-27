5da6d4807f32eabb12948c21ead0648d4a9bf699
Jonas Salo <salomannen1234@gmail.com>
2026-06-28 01:01:27 +0200

    sm8850-common: export camera processing vendor libs
    Change-Id: I4b3503cc05f28f668573c788881f246ebf7ad18d

--- extract-files.py
+++ extract-files.py
@@ -166,6 +166,17 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbase.so'),
     'vendor/usr/keylayout/gpio-keys.kl': blob_fixup()
         .add_line_if_missing('key 735   ASSIST'),
+    'vendor/etc/public.libraries.txt': blob_fixup()
+        .add_line_if_missing('libarcsoft_hdr_couple_api.so')
+        .add_line_if_missing('libarcsoft_high_dynamic_range_couple.so')
+        .add_line_if_missing('libarcsoft_smart_denoise.so')
+        .add_line_if_missing('libarcsoft_turbo_hdr_raw.so')
+        .add_line_if_missing('libarcsoft_turbo_raw.so')
+        .add_line_if_missing('libarcsoft_qnnhtp.so')
+        .add_line_if_missing('libQnnHtp.so')
+        .add_line_if_missing('libQnnSystem.so')
+        .add_line_if_missing('libQnnHtpV81Stub.so')
+        .add_line_if_missing('libQnnGpu.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
